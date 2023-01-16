73e1727aa0032dc672cbb960738979b3a6a007d3
Johnny Chen <Johnnychen384@gmail.com>
2023-01-16 15:53:39 -0500

    added cors stuff

--- src/main/java/com/example/Task_Manager_Api/TaskManagerApiApplication.java
+++ src/main/java/com/example/Task_Manager_Api/TaskManagerApiApplication.java
@@ -1,10 +1,16 @@
 package com.example.Task_Manager_Api;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskManagerApiApplication {
+public class TaskManagerApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TaskManagerApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApiApplication.class, args);
