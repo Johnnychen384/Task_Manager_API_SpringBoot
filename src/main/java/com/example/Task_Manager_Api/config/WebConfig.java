656750a9f6195035b2d3bcc5edd76ccbfebd8b87
Johnny Chen <Johnnychen384@gmail.com>
2023-01-16 16:01:08 -0500

    more cors stuff

--- src/main/java/com/example/Task_Manager_Api/config/WebConfig.java
+++ src/main/java/com/example/Task_Manager_Api/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.Task_Manager_Api;
+package com.example.Task_Manager_Api.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
